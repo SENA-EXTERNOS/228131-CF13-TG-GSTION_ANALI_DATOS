a39400295acbefe657e0ee2f818a5331ff9ed8a6
veimarcm <veimar.celis@gmail.com>
2022-11-22 15:04:01 -0500

    Actualizacion final
    Se actualiza el ajuste de cajon de texto

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,7 +13,7 @@
       .col-md-10.mb-md-0
         .px-2
           p.mb-0 Querido aprendiz, sea bienvenido a este componente formativo, donde podrá adquirir conocimientos relacionados con la ciencia de datos y cómo aplicarla en su organización. Aquí conocerá la estructura básica de un proyecto de ciencia de datos, así como las principales técnicas de análisis y clasificación de información usadas en la actualidad. Aprenderá conceptos básicos de #[i Big Data], datos abiertos y posibles fuentes de información para los proyectos a ejecutar.
-        .cajon-b.color-primario.p-3.mb-5.abajo-derecha
+        .cajon-b.cajon-x.color-primario.p-3.mb-5.abajo-derecha
           p.mb-0 En el siguiente enlace encontrará el video de introducción para que se relacione con la temática que se trabajará durante esta etapa formativa.
           p.mb-0 #[b ¡Muchos éxitos en la culminación de este componente formativo!].
 
